cbd8ca4fdeb4319939029c28d6505a831da601ba
Zhanglong Cao <279302D@curtin.edu.au>
2021-04-08 11:23:58 +0800

    simu file

--- R/SimuData.R
+++ R/SimuData.R
@@ -2,10 +2,19 @@
 #'
 #' @description This function takes *noise-free* velocity data
 #' computes the implied position data and then adds noise.
-#' It requires function forward1.
+#' It requires function `forward1`.
 #' @keywords V-Spline basis functions#'
 #' @param v noise-free velocity data
 #' @param rsnr signal to noise ratio
+#' @example 
+#' library(waveband)
+#' library(VSPline)
+#' set.seed(2016)
+#' N = 100
+#' velocity <- test.data(type = "blocks", n = N, signal = 1, rsnr = 7, plotfn = TRUE)
+#' set.seed(2016)
+#' position <- SimuData(velocity$y,7)
+#' simulatedata<- data.frame(t=velocity$x,y=position$xnoise,v=velocity$ynoise,boom=0)
 #' SimuData()
 SimuData = function(v,rsnr) {
   n  = length(v)
